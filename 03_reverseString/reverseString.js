cbff3a8bd8d29b25fd1bb14460d808f2db747890
SkipperShark <sharmanfaridz@gmail.com>
2024-02-02 11:52:43 +0800

    complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function (stringToReverse) {
+    return stringToReverse.split("").reverse().join("");
 
+    let reversedString = "";
+    let stringToReverseLastIndex = stringToReverse.length - 1;
+
+    for (let i = stringToReverseLastIndex; i >= 0; i--) {
+        reversedString += stringToReverse[i];
+    }
+
+    return reversedString
 };
 
 // Do not edit below this line
