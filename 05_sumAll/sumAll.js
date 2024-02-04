9235e6cb6ab9ac62a315e01bc0398a4e36e3f3a4
SkipperShark <sharmanfaridz@gmail.com>
2024-02-04 11:45:05 +0800

    complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
+const sumAll = function (intA, intB) {
 
+    if ((intA < 0) || (intB < 0)) {
+        return "ERROR";
+    }
+
+    if (!Number.isInteger(intA) || !Number.isInteger(intB)) {
+        return "ERROR";
+    }
+
+    [startVar, endVar] = [intA, intB];
+
+    if (intA > intB) [startVar, endVar] = [intB, intA];
+
+    let sum = null;
+
+    for (let i = startVar; i <= endVar; i++) {
+        sum += i;
+    }
+
+    return sum
+
+    // take 2 integer parameters
+    // if any of the parameters are positive integers, return "ERROR"
+    // start variable should be larger of both params
+    // end variable should be smaller of both params
+    // initialize a starter variable
+    // loop from first param (inclusive) to second param (inclusive)
+    // at each iteration, add i to the starter variable
+    // return the starter variable
 };
 
 // Do not edit below this line
