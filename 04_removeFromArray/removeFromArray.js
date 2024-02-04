8fe0d8fac9d4982920f7dbddf2c478b32e353f05
SkipperShark <sharmanfaridz@gmail.com>
2024-02-04 11:25:52 +0800

    complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,29 +1,7 @@
 const removeFromArray = function (array, ...elementsToRemove) {
 
-    return array.filter((element) => {
-        if (elementsToRemove.includes(element)) {
-            return false;
-        }
-        return true
-    })
-
-    for (let i = 0; i < array.length; i++) {
-
-        let curElement = array[i];
-
-        for (value in elementsToRemove) {
-
-        }
-
-        for (value in elementsToRemove) {
-            if (curElement === value) {
-                array.splice()
-            }
-        }
-    }
-
-
-};
+    return array.filter(element => !elementsToRemove.includes(element));
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
