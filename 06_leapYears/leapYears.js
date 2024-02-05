940c45bec1117215111e8037435b201c90348db4
sharman <sharmanfaridz@gmail.com>
2024-02-05 19:52:36 +0800

    complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,24 @@
-const leapYears = function() {
+const leapYears = function (year) {
 
+    yearDivisibleBy4 = year % 4 === 0;
+    yearDivisibleBy100 = year % 100 === 0;
+    yearDivisibleBy400 = year % 400 === 0;
+
+    if (yearDivisibleBy4) {
+        if (yearDivisibleBy100 && yearDivisibleBy400) {
+            return true;
+        }
+        else if (yearDivisibleBy100 && !yearDivisibleBy400) {
+            return false;
+        }
+        return true;
+    }
+
+    return false;
+    // input is a year
+    // years divisible by 400 are leap years
+    // years divisibly by 100 are not lear years
+    // years divisible by 4 are leap years
 };
 
 // Do not edit below this line
