49267876b6ac4cff61d8c4af646041a1dc4f93ef
sharman <sharmanfaridz@gmail.com>
2024-03-07 16:11:37 +0800

    complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (input) {
 
+    let string = input.replaceAll(" ", "").replaceAll(",", "").replaceAll(".", "").replaceAll("!", "");
+    string = string.toLowerCase();
+    console.log({ string });
+
+    let lastIndex = string.length - 1;
+
+    for (let i = 0; i < string.length; i++) {
+        let curChar = string[i];
+        let comparedChar = string[lastIndex - i];
+        console.log({ i, curChar, comparedChar });
+        if (curChar != comparedChar) {
+            return false;
+        }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
