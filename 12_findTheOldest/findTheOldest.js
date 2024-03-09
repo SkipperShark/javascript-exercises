531b970faa25fc5b7817b66492f338f44ab9fca1
sharman <sharmanfaridz@gmail.com>
2024-03-09 21:22:16 +0800

    complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
 
+    return people.sort((person1, person2) => {
+        let [person1Age, person2Age] = [getPersonAge(person1), getPersonAge(person2)];
+        if (person1Age < person2Age) return 1
+        else if (person1Age > person2Age) return -1
+    })[0]
 };
+
+function getPersonAge(person) {
+    let curYear = (new Date()).getFullYear();
+    return !person.yearOfDeath ? (curYear - person.yearOfBirth) : person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
