4cd08dd52c8cc5f3672ed135f005a020d740efd2
sharman <sharmanfaridz@gmail.com>
2024-03-09 21:08:32 +0800

    complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
 
+    if (typeof number === "string") {
+        number = parseInt(number);
+    }
+
+    if (number === 0) return 0;
+    else if (number === 1) return 1;
+    else if (number < 0) return "OOPS";
+
+
+    return fibonacci(number - 1) + fibonacci(number - 2);
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
